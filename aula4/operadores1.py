3b1ed6525b39b9ba2fcf8b3d1cf3939d50fbf0a6
Paulo Campanha <paulocampanha@gmail.com>
2025-09-03 10:23:23 -0300

    Aula 4 adicionada

---
+++ aula4/operadores1.py
@@ -0,0 +1,55 @@
+# Nesse programa vamos estudar os operadores aritméticos
+# do python
+
+num1 = 30
+num2 = 3
+
+# Soma (+)
+soma = num1 + num2
+print('A soma de', num1, 'com', num2, 'e', soma,'.')
+
+# Subtração (-)
+sub1 = num1 - num2
+sub2 = num2 - num1
+print('A subtração de ', num1, 'por', num2, 'é', sub1,'.')
+print('A subtração de ', num2, 'por', num1, 'é', sub2,'.')
+
+# Multiplicação (*)
+mult = num1 * num2
+print('A multiplicação de', num1 ,'por', num2 ,'é', mult ,'.')
+
+# Divisão (/) 
+# Obs: A divisão em Python sempre retorno um valor do tipo float
+div1 = num1 / num2
+div2 = num2 / num1
+print('A divisão de', num1, 'por', num2, 'é', div1,'.')
+print('A divisão de', num2, 'por', num1, 'é', div2,'.')
+
+# Divisão Inteira (//)
+# Obs: A divisão inteira retorna um valor inteiro
+div3 = num1 // num2
+div4 = num2 // num1
+print('A divisão inteira de', num1, 'por', num2, 'é', div3,'.')
+print('A divisão inteira de', num2, 'por', num1, 'é', div4,'.')
+
+# Modulo (resto da divisão)
+mod1 = num1 % num2
+mod2 = num2 % num1
+print('O resto da divisão de', num1,'por',num2,'é',mod1,'.')
+print('O resto da divisão de', num2,'por',num1,'é',mod2,'.')
+
+# Potencia
+potencia = num1 ** num2  
+print(num1,'elevado a',num2,'é',potencia,'.')
+
+# Porcentagem
+valor = 500
+porc = 0.1
+desconto = valor * porc  # 500 * 0.1 = 50
+valor_desconto = valor - desconto  # 500 - 50
+print('Valor com desconto: ', valor_desconto)
+
+salario = 5000
+porc_aumento = 0.15
+novo_salario = salario + (salario * porc_aumento)
+print('Novo salario: ', novo_salario)
---
+++ aula4/operadores2.py
@@ -0,0 +1,72 @@
+<<<<<<< HEAD
+# Nesse programa vamos estudar os operadores de atribuição
+# do Python
+
+nome = 'Gaspar'  # atribuindo a literal 'Gaspar' à variavel 'nome'
+
+valor = 10  # atribuindo a literal '10' à variavel 'valor'
+
+valor += 5  # equivalente à valor = valor + 5
+print(valor)
+print('-' * 30)
+
+valor -= 7 # equivalente à valor = valor - 7
+print(valor)
+print('-' * 30)
+
+valor *= 4  # equivalente à valor = valor * 4
+print(valor)
+print('-' * 30)
+
+valor /= 3  # equivalente à valor = valor / 3
+print(valor)
+print('-' * 30)
+
+valor **= 2 # equivalente à valor = valor ** 2
+print(valor)
+print('-' * 30)
+
+valor //= 5 # equivalente à valor = valor // 5
+print(valor)
+print('-' * 30)
+
+
+c = 26
+cel = str(c)
+=======
+# Nesse programa vamos estudar os operadores de atribuição
+# do Python
+
+nome = 'Gaspar'  # atribuindo a literal 'Gaspar' à variavel 'nome'
+
+valor = 10  # atribuindo a literal '10' à variavel 'valor'
+
+valor += 5  # equivalente à valor = valor + 5
+print(valor)
+print('-' * 30)
+
+valor -= 7 # equivalente à valor = valor - 7
+print(valor)
+print('-' * 30)
+
+valor *= 4  # equivalente à valor = valor * 4
+print(valor)
+print('-' * 30)
+
+valor /= 3  # equivalente à valor = valor / 3
+print(valor)
+print('-' * 30)
+
+valor **= 2 # equivalente à valor = valor ** 2
+print(valor)
+print('-' * 30)
+
+valor //= 5 # equivalente à valor = valor // 5
+print(valor)
+print('-' * 30)
+
+
+c = 26
+cel = str(c)
+>>>>>>> d494bef3bb3d11a4c319d3656caba6aa5be850e6
+print('Temp: ', cel + '°')
